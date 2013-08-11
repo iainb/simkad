bec6641670c72a4aa7bee990f2c9e270ffc12ee5
Iain Bullard <iain.bullard@gmail.com>
2013-08-11 18:30:00 +0100

    test simulated network update (check point, scruffy changes)

--- test.py
+++ test.py
@@ -9,9 +9,12 @@ import random
 def client_actions(client, nodes):
     count =  0
     stored = {}
+    gevent.sleep(random.randint(0,30))
     client._join_network(nodes)
+
     while True:
         gevent.sleep(5)
+        '''
         if random.random() < 0.25:
             key = '%s_%s' % (client.node.id, count)
             value = str(count)
@@ -26,14 +29,39 @@ def client_actions(client, nodes):
                 print 'ok good job'
             else:
                 print 'failed, expected %s, found %s' % (stored[key], value)
+        '''
 
+def client_summary(clients):
+    def bucket_stats(c):
+        mx = None
+        mi = None
+        for cl in c:
+            b = len(cl.rpc_client.routing.buckets)
+            #print cl.rpc_client.routing.buckets
+            if mx == None or b > mx:
+                mx = b
+            if mi == None or b < mi:
+                mi = b
+        return mx, mi
+
+
+    print "client_summary"
+    while True:
+        gevent.sleep(5)
+        bmax, bmin = bucket_stats(clients)
+        print "client_summary, watching %s clients" % (len(clients))
+        print "min_buckets: %s, max_buckets %s" % (bmin, bmax)
 
 def spawn_clients(pool, network, n):
+    clients = []
     last, kad_client = spawn_client(pool, network)
+    clients.append(kad_client)
     for i in xrange(0,n-1):
         last, kad_client = spawn_client(pool, network, last)
+        clients.append(kad_client)
 
     print "test: spawned %s nodes" % n
+    pool.spawn(client_summary, clients)
 
 def spawn_client(pool, network, initial_node=None):
     if initial_node is None:
@@ -48,7 +76,7 @@ def spawn_client(pool, network, initial_node=None):
     pool.spawn(client_actions, kad_client, nodes)
     return node, kad_client
 
-p = pool.Pool(6000)
+p = pool.Pool(100000)
 network = Simulate(False)
-p.spawn(spawn_clients, p, network, 100)
+p.spawn(spawn_clients, p, network, 5000)
 p.join()
